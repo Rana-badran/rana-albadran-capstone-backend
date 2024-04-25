12d9bd60a17ebe08e4dc52cd9f053f8b5d694085
rana-badran <rana.albadran@gmail.com>
2024-04-25 16:33:35 -0400

    initial seeds and migration files

---
+++ migrations/20240424211258_stores.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("stores", function (table) {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.string("address").notNullable();
+    table.string("media");
+    table.json("categories");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("stores");
+};
