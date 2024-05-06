2895f1bd6fce4e89e1a2f144bc19e6dafca61a97
rana-badran <rana.albadran@gmail.com>
2024-05-06 13:13:14 -0400

    added longitude and latitude to migration and seeds

--- migrations/20240424211258_stores.js
+++ migrations/20240424211258_stores.js
@@ -3,6 +3,8 @@ exports.up = function (knex) {
     table.increments("id");
     table.string("name").notNullable();
     table.string("address").notNullable();
+    table.string("latitude");
+    table.string("longitude");
     table.string("media");
     table.json("categories");
   });
